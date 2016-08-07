9840f0604aa40a592787256511221e9f4fe115d2
Dafydd Jones <dafydd@techneg.it>
2016-08-08 00:34:19 +0100

    update package list regardless & install apt-utils to aid debconf & shutdown after minute delay to prevent packer hanging

--- script/update.sh
+++ script/update.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -eux
 
-if [[ $UPDATE  =~ true || $UPDATE =~ 1 || $UPDATE =~ yes ]]; then
-	echo "==> Updating list of repositories"
-    # apt-get update does not actually perform updates, it just downloads and indexes the list of packages
-    apt-get -y update
+echo "==> Updating list of repositories"
+# apt-get update does not actually perform updates, it just downloads and indexes the list of packages
+apt-get -y update
+apt-get -y install apt-utils
 
+if [[ $UPDATE  =~ true || $UPDATE =~ 1 || $UPDATE =~ yes ]]; then
     echo "==> Performing dist-upgrade (all packages and kernel)"
     apt-get -y dist-upgrade --force-yes
-    reboot
-    sleep 60
+
+    shutdown -r
 fi

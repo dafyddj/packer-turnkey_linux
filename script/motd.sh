ad648c84a3e647ef6f5177c61f301266c3535393
Dafydd Jones <dafydd@techneg.it>
2016-08-09 23:59:48 +0100

    use update-motd for custom motd

--- script/motd.sh
+++ script/motd.sh
@@ -4,13 +4,13 @@ echo "==> Recording box generation date"
 date > /etc/vagrant_box_build_date
 
 echo "==> Customizing message of the day"
-MOTD_FILE=/etc/motd
-BANNER_WIDTH=64
-PLATFORM_RELEASE=$(lsb_release -sd)
-PLATFORM_MSG=$(printf '%s' "$PLATFORM_RELEASE")
-BUILT_MSG=$(printf 'built %s' $(date +%Y-%m-%d))
-printf '%0.1s' "-"{1..64} > ${MOTD_FILE}
-printf '\n' >> ${MOTD_FILE}
-printf '%2s%-30s%30s\n' " " "${PLATFORM_MSG}" "${BUILT_MSG}" >> ${MOTD_FILE}
-printf '%0.1s' "-"{1..64} >> ${MOTD_FILE}
-printf '\n' >> ${MOTD_FILE}
+MOTD_FILE=/etc/update-motd.d/04-packer-build-info
+
+cat << 'EOF' > ${MOTD_FILE}
+#! /bin/sh
+
+echo "  Box built on $(cat /etc/vagrant_box_build_date)"
+echo
+EOF
+
+chmod +x ${MOTD_FILE}

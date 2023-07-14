18e90c91dbab3011a1b0ffe0a462979a573b303b
Dafydd Jones <dafydd@techneg.it>
2023-07-14 23:21:53 +0100

    fix: perl not required

--- script/virtualbox.sh
+++ script/virtualbox.sh
@@ -2,8 +2,7 @@
 
 if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
     echo "==> Installing VirtualBox guest additions"
-    apt-get update
-    apt-get install -y linux-headers-$(uname -r) build-essential perl
+    apt-get install -y linux-headers-$(uname -r) build-essential
     apt-get install -y dkms
 
     mount -o loop VBoxGuestAdditions.iso /mnt

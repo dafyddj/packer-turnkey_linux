c592dbc7f96f283dbf95edbfcea2c9e3d5f5bda4
Dafydd Jones <dafydd@techneg.it>
2023-07-18 16:48:39 +0100

    feat: build & upload TKL v16 with GitHub Actions (#2)

--- script/virtualbox.sh
+++ script/virtualbox.sh
@@ -2,13 +2,11 @@
 
 if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
     echo "==> Installing VirtualBox guest additions"
-    apt-get install -y linux-headers-$(uname -r) build-essential perl
+    apt-get install -y linux-headers-$(uname -r) build-essential
     apt-get install -y dkms
 
-    VBOX_VERSION=$(cat .vbox_version)
-    mount -o loop VBoxGuestAdditions_${VBOX_VERSION}.iso /mnt
+    mount -o loop VBoxGuestAdditions.iso /mnt
     sh /mnt/VBoxLinuxAdditions.run --nox11
     umount /mnt
-    rm VBoxGuestAdditions_${VBOX_VERSION}.iso
-    rm .vbox_version
+    rm VBoxGuestAdditions.iso
 fi
